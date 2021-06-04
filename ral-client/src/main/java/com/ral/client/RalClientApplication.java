14ca7900613267a65579bcb13378f9c9d5f92e5a
游泽灵 <youzl@xmpaoyou.com>
2021-06-04 14:50:34 +0800

    游戏框架

---
+++ ral-client/src/main/java/com/ral/client/RalClientApplication.java
@@ -0,0 +1,13 @@
+package com.ral.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RalClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RalClientApplication.class, args);
+	}
+
+}
---
+++ ral-server/src/main/java/com/ral/server/RalServerApplication.java
@@ -0,0 +1,13 @@
+package com.ral.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RalServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RalServerApplication.class, args);
+	}
+
+}
